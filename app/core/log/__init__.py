35193b0780e627c8e84efa7658fc057bcea4177b
scrazy_akai <2145303367@qq.com>
2026-04-30 14:00:11 +0800

    feat:日志中间件

--- app/core/log/__init__.py
+++ app/core/log/__init__.py
@@ -1,6 +1,7 @@
 from app.core.log.config import configure_logging, is_logging_configured
 from app.core.log.loggers import (
     get_access_logger,
+    get_exception_logger,
     get_logger,
 )
 from app.core.log.middleware import AccessLogMiddleware
@@ -9,6 +10,7 @@ __all__ = [
     "AccessLogMiddleware",
     "configure_logging",
     "get_access_logger",
+    "get_exception_logger",
     "get_logger",
     "is_logging_configured",
 ]
